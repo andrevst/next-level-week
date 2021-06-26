c2e1219aec86103215b7e4f60e7ac1247dbe23be
andrevst <andreveigatonelli@gmail.com>
2021-06-26 03:45:56 -0300

    class 4: node, make a compliment

--- nlwValoriza/src/routes.ts
+++ nlwValoriza/src/routes.ts
@@ -2,14 +2,19 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/createUserController";
 import { CreateTagController } from "./controllers/createTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { CreateComplimentController } from "./controllers/createComplimentController";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const createComplimentController = new CreateComplimentController();
+
 
 router.post("/users", createUserController.handle);
 
 router.post("/tags", ensureAdmin, createTagController.handle);
+
+router.post("/compliments", createComplimentController.handle);
 
 
 export { router };

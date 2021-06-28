3aea040bca6fd8476828e449086a3d32bb3753c2
andrevst <andreveigatonelli@gmail.com>
2021-06-28 04:22:39 -0300

    class: node 5, finish tag api

--- nlwValoriza/src/routes.ts
+++ nlwValoriza/src/routes.ts
@@ -4,19 +4,21 @@ import { CreateTagController } from "./controllers/createTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { CreateComplimentController } from "./controllers/createComplimentController";
 import { AuthenticateUserController } from "./controllers/authenticateUserController";
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListTagsController } from "./controllers/listTagController";
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const createComplimentController = new CreateComplimentController();
 const authenticateUserController = new AuthenticateUserController();
-import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+const listTagsController = new ListTagsController();
 
 
 router.post("/users", createUserController.handle);
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
-// router.get("/tags", ensureAuthenticated, listTagsController.handle);
+router.get("/tags", ensureAuthenticated, listTagsController.handle);
 
 router.post("/compliments", createComplimentController.handle);
 
